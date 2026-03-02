a10ff92cd75080784b31cdbf8510ef1973f878cd
SongGeunHo <104110883+SongGunHo@users.noreply.github.com>
2026-03-02 14:48:48 +0900

    init

---
+++ src/main/java/com/it/Mujakjung_be/MujakjungBeApplication.java
@@ -0,0 +1,13 @@
+package com.it.Mujakjung_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MujakjungBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MujakjungBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/it/Mujakjung_be/MujakjungBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.it.Mujakjung_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MujakjungBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
