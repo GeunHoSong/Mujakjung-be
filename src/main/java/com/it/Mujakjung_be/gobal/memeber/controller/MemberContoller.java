031fe6f4c9786b7ee0d34809f206f1c099302172
SongGeunHo <104110883+SongGunHo@users.noreply.github.com>
2026-03-03 16:54:12 +0900

    회원 가입 만드는 중

---
+++ src/main/java/com/it/Mujakjung_be/gobal/memeber/controller/MemberContoller.java
@@ -0,0 +1,11 @@
+package com.it.Mujakjung_be.gobal.memeber.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/member")
+public class MemberContoller {
+}
