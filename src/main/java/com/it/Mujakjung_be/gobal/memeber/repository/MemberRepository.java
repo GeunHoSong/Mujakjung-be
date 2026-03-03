a0fbf316cafdaecfcbffdf95c395c57170a64273
SongGeunHo <104110883+SongGunHo@users.noreply.github.com>
2026-03-03 16:56:37 +0900

    구도 만드는 중

---
+++ src/main/java/com/it/Mujakjung_be/gobal/memeber/repository/MemberRepository.java
@@ -0,0 +1,7 @@
+package com.it.Mujakjung_be.gobal.memeber.repository;
+
+import com.it.Mujakjung_be.gobal.memeber.entity.MemberEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
+}
---
+++ src/main/java/com/it/Mujakjung_be/gobal/memeber/service/MemberService.java
@@ -0,0 +1,7 @@
+package com.it.Mujakjung_be.gobal.memeber.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberService {
+}
