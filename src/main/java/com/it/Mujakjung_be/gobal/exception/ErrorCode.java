eb443e093322ac730f151e7aae9c3f868a995d49
SongGeunHo <104110883+SongGunHo@users.noreply.github.com>
2026-03-17 15:53:36 +0900

    error page save

--- src/main/java/com/it/Mujakjung_be/gobal/exception/ErrorCode.java
+++ src/main/java/com/it/Mujakjung_be/gobal/exception/ErrorCode.java
@@ -1,4 +1,24 @@
 package com.it.Mujakjung_be.gobal.exception;
 
-public class ErrorCode {
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+public enum ErrorCode {
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘 못된 요청입니다");
+
+    private final HttpStatus status;
+    private final String message;
+
+    ErrorCode(HttpStatus status ,String message){
+        this.status = status;
+        this.message = message;
+    }
+
+    public HttpStatus getStatus (){
+        return status;
+    }
+
+    public String getMessage(){
+        return message;
+    }
 }
--- src/main/java/com/it/Mujakjung_be/gobal/exception/GlobalExceptionHandler.java
+++ src/main/java/com/it/Mujakjung_be/gobal/exception/GlobalExceptionHandler.java
@@ -1,4 +1,14 @@
 package com.it.Mujakjung_be.gobal.exception;
 
-public class GlobalExceptionHandler {
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler{
+
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<String> handleCustomException(CustomException e){
+        return ResponseEntity.status(e.getErrorCode().getStatus()).body(e.getErrorCode().getMessage());
+    }
 }
