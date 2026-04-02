fc206e6eee4c2cfafd3204a827e8483d47b6df75
SongGeunHo <104110883+SongGunHo@users.noreply.github.com>
2026-04-02 16:27:23 +0900

    카카오 로그인 성공

--- src/main/java/com/it/Mujakjung_be/gobal/kakao/controller/KakaoController.java
+++ src/main/java/com/it/Mujakjung_be/gobal/kakao/controller/KakaoController.java
@@ -18,7 +18,14 @@ public class KakaoController {
     private final KakaoService service;
 
     @GetMapping("/callback")
-    public void kakaoCallback(@RequestParam String code, HttpServletResponse response) throws IOException {
+    public void kakaoCallback(@RequestParam(value = "code", required = false)String code
+    , HttpServletResponse response) throws IOException {
+        // 만약 인가 코드
+        if(code == null){
+            System.out.println("인가 코드가 없습니다 메인으로 돌아 갑니다 ");
+            response.sendRedirect("http://localhost:5173/");
+            return;
+        }
         // 1. 인가 코드로 access token 받기
         String accessToken = service.getAccessToken(code);
 
@@ -30,6 +37,6 @@ public class KakaoController {
 
         // 4. ⭐ 직접 리다이렉트 시키기 (메인 화면으로!)
         // 따로 메서드 만들 필요 없이 response 객체가 가진 기능을 호출만 하면 돼.
-        response.sendRedirect("http://localhost:3000");
+        response.sendRedirect("http://localhost:5173/?login=success");
     }
 }
---
+++ src/main/java/com/it/Mujakjung_be/gobal/kakao/dto/Profile.java
@@ -0,0 +1,9 @@
+package com.it.Mujakjung_be.gobal.kakao.dto;
+
+import lombok.Data;
+
+@Data
+public  class Profile {
+    private String nickname;
+    private String profile_image_url;
+}
