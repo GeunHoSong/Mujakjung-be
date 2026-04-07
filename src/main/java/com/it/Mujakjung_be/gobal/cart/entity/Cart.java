1afdfdf33737933e95d3ea10f70729de240f304d
SongGeunHo <104110883+SongGunHo@users.noreply.github.com>
2026-04-07 18:56:28 +0900

    만든는 중

--- src/main/java/com/it/Mujakjung_be/gobal/cart/entity/Cart.java
+++ src/main/java/com/it/Mujakjung_be/gobal/cart/entity/Cart.java
@@ -1,10 +1,13 @@
 package com.it.Mujakjung_be.gobal.cart.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.lang.reflect.Member;
+import java.time.LocalDateTime;
 
 @Entity
 @Setter
@@ -12,4 +15,36 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long cartId;
+
+    // 회원 엔티티 와 직접 연결 (가장 중요)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_email", referencedColumnName = "email")
+    private Member member;
+
+    // 상품 정보
+    @Column(nullable = false)
+    private String productName;
+
+    private int productPrice; // 상품 가격
+
+    private int quantity = 1;
+
+    // 데이터 생성시 시간 자동 반복
+    private LocalDateTime addedAt;
+
+
+    @PrePersist
+    public void prePersist(){
+        this.addedAt = LocalDateTime.now();
+    }
+
+
+
+
+
+
+
 }
