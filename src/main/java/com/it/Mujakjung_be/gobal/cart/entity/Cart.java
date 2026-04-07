a171a155a321e1a56432b41d91dddef9d47fdb5a
SongGeunHo <104110883+SongGunHo@users.noreply.github.com>
2026-04-07 18:50:26 +0900

    장바 구니 만드 는 중

---
+++ src/main/java/com/it/Mujakjung_be/gobal/cart/entity/Cart.java
@@ -0,0 +1,15 @@
+package com.it.Mujakjung_be.gobal.cart.entity;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Cart {
+}
