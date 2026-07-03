27a800168b8d10dd00d8634ea843db2627d3dc33
SongGeunHo <104110883+SongGunHo@users.noreply.github.com>
2026-07-03 19:02:17 +0900

    mypage id 값 하느 는 중

--- src/main/java/com/it/Mujakjung_be/global/mypage/controller/MyPageController.java
+++ src/main/java/com/it/Mujakjung_be/global/mypage/controller/MyPageController.java
@@ -1,10 +1,12 @@
 package com.it.Mujakjung_be.global.mypage.controller;
 
+import com.it.Mujakjung_be.global.mypage.dto.MyPageDto;
+import com.it.Mujakjung_be.global.mypage.entity.MyPageEntity;
 import com.it.Mujakjung_be.global.mypage.service.MyPageService;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -12,4 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/mypage")
 public class MyPageController {
     private final MyPageService service;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<MyPageDto> getMyPage(@PathVariable  Long id){
+        MyPageDto mypage = service.getMypage(id);
+        return ResponseEntity.ok(mypage);
+    }
+
+
+
 }
